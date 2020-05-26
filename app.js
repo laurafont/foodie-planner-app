916ffd69484033f4c2741ea6c9d1b45dbfa61319
Laura Font <laurafontsalvador@gmail.com>
2020-05-26 21:03:54 +0200

    heroku changes

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get("/", function (req, res, next) {
-  res.send("Welcome to the FOODIEPLANNER backend");
-});
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
